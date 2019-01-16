f2fe4ce19e315fa819365fa6323ce4f6e9d2753e
Alex Poon <chpoon92@users.noreply.github.com>
2019-01-17 00:37:55 +0800

    Create brew-install-script.sh

---
+++ brew-install-script.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "Install brew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# browsers
+brew cask install chromium
+brew cask install firefox
+brew cask install google-chrome
+
+# communication
+brew cask install telegram-desktop
+brew cask install whatsapp
+
+# dev tools
+brew cask install docker
+brew install git
+brew cask install iterm2
+brew install kubernetes-cli
+brew cask install postman
+brew cask install sourcetree
+brew cask install visual-studio-code
+
+# entertainment
+brew cask install calibre
+brew cask install openemu
+brew cask install origin
+brew cask install steam
+
+# programming
+brew install python3
+brew cask install anaconda
+brew install nvm
+echo "source $(brew — prefix nvm)/nvm.sh" >> .bash_profile
+
+# tools
+brew cask install gimp
+brew install noclamshell
+brew cask install parallels
+brew cask install scroll-reverser
+brew cask install the-unarchiver
