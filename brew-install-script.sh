f7f31389692606fdd4a4419f5b48d4f6e4409a02
Alex Poon <chpoon92@gmail.com>
2020-02-01 04:39:36 -0800

    Remove unused package and fix

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -7,11 +7,9 @@ echo "Disable analytics"
 brew analytics off
 
 # add cask
-brew tap caskroom/cask
 brew tap homebrew/cask-drivers
 
 # browsers
-brew cask install chromium
 brew cask install firefox
 brew cask install google-chrome
 
@@ -22,27 +20,21 @@ brew cask install telegram-desktop
 brew cask install whatsapp
 
 # dev tools
-brew cask install bluestacks
 brew cask install docker
 brew install git
 brew install git-lfs
 git lfs install
-brew cask install iterm2
 brew cask install intellij-idea-ce
 brew cask install kitematic
-brew install kubernetes-cli
 brew cask install postman
 brew cask install sourcetree
 brew cask install vagrant
-brew cask install virtualbox
 brew cask install visual-studio-code
 
 # entertainment
 brew cask install calibre
 brew cask install openemu
-brew cask install origin
 brew cask install steam
-brew cask install vlc
 
 # programming
 brew install pyenv
@@ -53,7 +45,7 @@ pyenv install anaconda3-2018.12
 pyenv global anaconda3-2018.12
 brew install nvm
 mkdir ~/.nvm
-printf 'export NVM_DIR="$HOME/.nvm"\n[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm\n[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion\n' >> .bash_profile
+printf 'export NVM_DIR="$HOME/.nvm"\n[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm\n[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion\n' >> ~/.zshrc
 nvm install --lts
 nvm use stable
 
@@ -62,15 +54,11 @@ brew cask install alfred
 brew cask install bitwarden
 brew cask install content-manager-assistant
 brew cask install dbeaver-community
-brew cask install energybar
 brew cask install evernote
 brew cask install gimp
-brew install pirj/homebrew-noclamshell/noclamshell
-brew services start noclamshell
 brew cask install nordvpn
 brew cask install oversight
 brew cask install parallels
 brew cask install pock
-brew cask install scroll-reverser
 brew cask install spectacle
 brew cask install the-unarchiver
