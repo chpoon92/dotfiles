9800549bf3421c7ef54db964c9217d735963fbea
Alex Poon <chpoon92@gmail.com>
2020-02-29 15:53:16 +0800

    Add mas script

---
+++ mas.sh
@@ -0,0 +1,15 @@
+# Microsoft Remote Desktop
+mas install 1295203466
+
+# LINE
+mas install 539883307
+
+# Mac Apps
+# Xcode
+mas install 497799835
+# Pages
+mas install 409201541
+# Keynote
+mas install 409183694
+# Numbers
+mas install 409203825
