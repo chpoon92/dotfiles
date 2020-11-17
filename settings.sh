f77ddf80a8f3341c805a9b76b83912bf493287a3
Alex Poon <chpoon92@tutanota.com>
2020-11-18 00:19:00 +0800

    Create Mac settings

---
+++ settings.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# ============
+# Dock
+# ============
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
